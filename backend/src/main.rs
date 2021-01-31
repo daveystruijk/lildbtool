0baf37a42875b7f855ec8f3784791695f24e8920
Davey Struijk <mail@daveystruijk.com>
2021-01-31 16:41:25 +0100

    Add blueprintjs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+extern crate postgres;
+extern crate dotenv;
 
 use std::io;
 use std::path::{Path, PathBuf};
-
+use std::env;
+use dotenv::dotenv;
 use rocket::response::NamedFile;
+use postgres::{Connection, TlsMode};
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -18,5 +22,35 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, files]).launch();
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL");
+
+    println!("Connecting to {}", database_url);
+
+    let pg = Connection::connect(database_url, TlsMode::None).unwrap();
+
+    // rocket::ignite()
+    //     .manage(pg)
+    //     .mount("/", routes![index, files])
+    //     .launch();
 }
+
+
+
+
+// struct Person {
+//     id: i32,
+//     name: String,
+//     data: Option<Vec<u8>>
+// }
+// 
+//     for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
+//         let person = Person {
+//             id: row.get(0),
+//             name: row.get(1),
+//             data: row.get(2)
+//         };
+//         println!("Found person {}", person.name);
+//     }
+// 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "normalize.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import '@blueprintjs/core/lib/css/blueprint.css';
 
-class App extends React.Component {
-  render() {
-    return <h1>My React App</h1>
-  }
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import { FocusStyleManager, Classes, Icon, Menu, MenuDivider, MenuItem, Navbar, Tab, Tabs, Alignment } from "@blueprintjs/core";
+FocusStyleManager.onlyShowFocusOnTabs();
+
+const App = () => {
+  const [navbarTabId, setNavbarTabId] = useState("home");
+
+  const handleNavbarTabChange = (navbarTabId) => {
+    setNavbarTabId(navbarTabId);
+  };
+
+  return <div>
+    <Tabs
+      animate={true}
+      large={true}
+      vertical={true}
+      onChange={handleNavbarTabChange}
+      selectedTabId={navbarTabId}
+    >
+      <Tab id="home" title="Home" />
+      <Tab id="files" title="Files" />
+      <Tab id="builds" title="Builds" />
+    </Tabs>
+  </div>;
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));

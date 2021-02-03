a57d2ceb58b47c2ccc773bb570c4a96049ac0923
Davey Struijk <mail@daveystruijk.com>
2021-02-03 14:39:35 +0100

    Use actix-web with sqlx

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,56 +1,73 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] extern crate rocket;
-extern crate postgres;
-extern crate dotenv;
-
-use std::io;
+use log::{debug, info, error};
 use std::path::{Path, PathBuf};
 use std::env;
 use dotenv::dotenv;
-use rocket::response::NamedFile;
-use postgres::{Connection, TlsMode};
+use actix_web::{web, App, HttpServer, HttpRequest, Result, middleware::Logger};
+use actix_files::NamedFile;
+use sqlx::postgres::PgPool;
 
-#[get("/")]
-fn index() -> io::Result<NamedFile> {
-    NamedFile::open("../frontend/build/index.html")
+
+/**
+ * Serve static files (at ../frontend/build/)
+ */
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open(Path::new("../frontend/build/index.html"))?)
 }
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("../frontend/build").join(file)).ok()
+async fn files(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(Path::new("../frontend/build").join(path))?)
 }
 
-fn main() {
+/**
+ * API
+ */
+
+// TODO:
+// - create side-table (on startup?)
+// 
+
+async fn tables(pool: web::Data<PgPool>) -> Result<()> {
+    let records = sqlx::query!(r#"
+        SELECT *
+        FROM pg_catalog.pg_tables
+        WHERE schemaname != 'pg_catalog' AND 
+        schemaname != 'information_schema';
+    "#).fetch_all(pool.get_ref()).await?;
+
+    for record in records {
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+/**
+ * Entrypoint
+ */
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
     dotenv().ok();
+    env::set_var("RUST_LOG", "info,actix_web=info");
+    env_logger::init();
 
-    let database_url = env::var("DATABASE_URL");
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL not set.");
 
-    println!("Connecting to {}", database_url);
+    info!("Connecting to {}", database_url);
+    let pool = PgPool::connect(&database_url).await?;
 
-    let pg = Connection::connect(database_url, TlsMode::None).unwrap();
+    let listen_addr = "127.0.0.1:8080";
+    HttpServer::new(move || App::new()
+            .wrap(Logger::default())
+            .data(pool.clone())
+            .route("/api/tables", web::get().to(tables))
+            .route("/", web::get().to(index))
+            .route("/{filename:.*}", web::get().to(files))
+        ).bind(listen_addr)?
+        .run()
+        .await?;
 
-    // rocket::ignite()
-    //     .manage(pg)
-    //     .mount("/", routes![index, files])
-    //     .launch();
+    Ok(())
 }
 
-
-
-
-// struct Person {
-//     id: i32,
-//     name: String,
-//     data: Option<Vec<u8>>
-// }
-// 
-//     for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
-//         let person = Person {
-//             id: row.get(0),
-//             name: row.get(1),
-//             data: row.get(2)
-//         };
-//         println!("Found person {}", person.name);
-//     }
-// 
